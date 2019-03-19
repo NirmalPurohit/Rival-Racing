d25d2b1214cbe09261342ced9e7230f7fe2382d5
NirmalPurohit <nirmalpurohit6538@gmail.com>
2019-03-19 13:25:49 -0400

    Update Spinning.js
    code refurbished

--- Assets/javascripts/Spinning.js
+++ Assets/javascripts/Spinning.js
@@ -2,22 +2,17 @@
 
 public var speed:float=1000;
 
-function Start()
-{
+function Start(){
 }
 
 function Update()
 {
-	//transform.RotateAround (Vector3., Vector3.up, 20 * Time.deltaTime);
-	if(Input.GetKey("w"))
-	{
-	transform.Rotate(Vector3.right, speed * Time.deltaTime*100);
-	speed+=Time.deltaTime;
+	if(Input.GetKey("w")){
+		transform.Rotate(Vector3.right, speed * Time.deltaTime*100);
+		speed+=Time.deltaTime;
 	}
-	if(Input.GetKey("s"))
-	{
-	transform.Rotate(Vector3.left, speed * Time.deltaTime*100);
-	speed+=Time.deltaTime;
+	if(Input.GetKey("s")){
+		transform.Rotate(Vector3.left, speed * Time.deltaTime*100);
+		speed+=Time.deltaTime;
 	}
-	
 }

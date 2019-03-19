e6b100fcb66ab9d89271526fb7c899c7f514d359
NirmalPurohit <nirmalpurohit6538@gmail.com>
2019-03-19 13:27:12 -0400

    Update turn_wheel.js
    code refurbished

--- Assets/javascripts/turn_wheel.js
+++ Assets/javascripts/turn_wheel.js
@@ -1,12 +1,15 @@
 ﻿#pragma strict
+
 public var turnSpeed : float = 50f;
 
 function Start () {
-
 }
 
 function Update () {
-
+	/*
+	 * Turn the wheels on a key press
+	 * along with the car movement
+	 */
 	if(Input.GetKey("right"))
 		transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
 		 
